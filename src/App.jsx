83da426e3a658b5fbdf3603afc6ff2437d6a4c1c
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-20 19:05:23 +0100

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Header from "./Header";
+
 function App() {
   return (
     <>
-      <p className="text-3xl">hello</p>
+      <div className="min-h-screen bg-[#23272f] text-gray-100 pt-8">
+        <Header />
+        <main className="container mx-auto p-4">
+          <p className="text-3xl">hello</p>
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import Logo from "./assets/logos_react.svg";
+
+function Header() {
+  return (
+    <header>
+      <div className="flex justify-center items-center p-4 gap-8">
+        <h1 className="text-4xl font-bold">REACT QUIZ</h1>
+        <img src={Logo} alt="React Logo" width="100" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
