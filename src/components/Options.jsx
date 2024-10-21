15a83ce1e694847be6eaf921d2740ab7fb336d30
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-21 15:02:50 +0100

    conditionally translate button dependent on picked answer

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -17,6 +17,7 @@ function Options({ question, dispatch, answer }) {
         // Determine button style based on current state
         const isCorrect = hasAnswered && index === correctOption;
         const isIncorrect = hasAnswered && index !== correctOption;
+        const pickedAnswer = hasAnswered && index === answer;
 
         return (
           <button
@@ -24,12 +25,13 @@ function Options({ question, dispatch, answer }) {
             className={`text-xl w-full p-2 rounded-lg transform transition duration-300 ease-in-out border-2 border-transparent
               ${
                 isCorrect
-                  ? "bg-[#59c5db] translate-x-2 shadow-lg border-[#59c5db]" // Correct answer styling
+                  ? "bg-[#59c5db] shadow-lg border-[#59c5db]" // Correct answer styling
                   : isIncorrect
                   ? "bg-orange-400 border-orange-500" // Incorrect answer styling
                   : "bg-[#323844] hover:border-[#59c5db] hover:bg-[#4E5460] hover:translate-x-2 hover:shadow-lg" // Default hover state
               }
-              ${hasAnswered ? "cursor-not-allowed" : "cursor-pointer"}`} // Disabled state
+              ${hasAnswered ? "cursor-not-allowed" : "cursor-pointer"}
+              ${pickedAnswer && "translate-x-2"}`}
             onClick={() => dispatch({ type: "newAnswer", payload: index })}
             disabled={hasAnswered} // Disable buttons once answered
           >
