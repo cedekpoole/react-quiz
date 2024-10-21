24c912a8e99d1cb34fec0efa83ea8880d7289a9a
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-21 19:06:16 +0100

    edit finished page

--- src/components/Finished.jsx
+++ src/components/Finished.jsx
@@ -7,17 +7,24 @@ Finished.propTypes = {
 
 function Finished({ points, pointsSum }) {
   const percentage = Math.ceil((points / pointsSum) * 100);
+
   return (
-    <div className="text-center mt-10 p-10">
-      <h1 className="text-4xl font-bold mb-4 font-albertsans">
-        Quiz Finished!
-      </h1>
-      <p className="text-xl">
-        You scored <span className="text-[#59c5db] font-bold">{points}</span>{" "}
-        out of <span className="text-[#59c5db]">{pointsSum}</span> points (
-        {percentage}
-        %).
-      </p>
+    <div className="flex flex-col items-center mt-10">
+      <div className="max-w-xl w-full text-center px-8 py-12 border border-gray-200 shadow-lg rounded-lg">
+        <h1 className="text-3xl font-semibold mb-4 font-albertsans ">
+          Quiz Finished!
+        </h1>
+        <p className="text-lg mb-6">
+          You scored <span className="text-[#59c5db] font-bold">{points}</span>{" "}
+          out of <span className="text-[#59c5db] font-bold">{pointsSum}</span>{" "}
+          points ({percentage}%).
+        </p>
+
+        {/* Button (optional for retry) */}
+        <button className="mt-4 px-5 py-2.5 bg-[#59c5db] text-[#20232a] font-medium rounded-md hover:bg-[#4fb3d3] transition-colors">
+          Retake Quiz
+        </button>
+      </div>
     </div>
   );
 }
