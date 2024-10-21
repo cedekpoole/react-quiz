fc19c54add952b65fc8dfad7bf736565e909a7d8
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-21 10:43:44 +0100

    install json-server package

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
     <>
       <div className="min-h-screen bg-[#23272f] text-gray-100 pt-8">
         <Header />
-        <main className="container mx-auto p-4">
-          <p className="text-3xl">hello</p>
+        <main className="container mx-auto p-10">
+          <p className="text-3xl">Question 1/15</p>
         </main>
       </div>
     </>
